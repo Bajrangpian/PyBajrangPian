e0f2aa7c02702e2a09df13c44b529b9eb4edccfd
12wpprog <bajrang.pian@gmail.com>
2023-11-06 10:58:01 +0700

    cpp testing

---
+++ abc.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num1;
+    cout<<"INput number :";
+    cin >> num1;
+    cout<<"Your number is : "<<num1;
+    return 0;
+
+    
+
+}
